087b7ce25988b3a98314d99d8ba7fc64a83f344d
sumeyraltas <sumeyraltas@gmail.com>
2023-11-06 12:20:40 +0300

    Initial Commit

---
+++ ToDoListExample/ToDoListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListExampleApp.swift
+//  ToDoListExample
+//
+//  Created by Sumeyra Altas on 6.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
